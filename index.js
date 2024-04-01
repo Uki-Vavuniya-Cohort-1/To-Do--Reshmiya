b2f11c1bd4455293a92c9fe31b8a034695f2a8b6
Resmiya <asirreshim0103@gmail.com>
2024-04-01 20:18:07 +0530

    added

--- index.js
+++ index.js
@@ -1,21 +1,128 @@
-const mongoose = require('mongoose')
+const Mongoose = require('mongoose');
+const express = require('express');
+const ToDo = require("./Shcema/Note.model.js");
+const User = require("./Shcema/User.model.js");
+const app = express();
+app.use(express.json());
 const URI = "mongodb+srv://asirreshmi0103:asir0103@cluster0.ivhu6sq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-const express = require('express');
-const { log } = require('console');
-const app = express();
-
-
-
-mongoose.connect(URI)
-    .then(() => {
-        console.log("Connected to the database!");
-    })
-    .catch((error) => {
-        console.log("Connection failed!", error);
-    });
-
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB')
+.then(()=>{
+  console.log("You are connected");
 })
+.catch((error)=>{
+  console.log("Connection failed",error)
+});
+
+app.listen(3000, () => {
+console.log("Server is running on port 3000");
+});
+
+app.post('/create', async (req, res) => {
+  try {
+    const toDo = new ToDo(req.body);
+    await toDo.save();
+    res.status(201).send(toDo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});      
+
+app.get('/view', async (req, res) => {
+  try {
+    const toDo = await ToDo.find();
+      res.status(200).json(toDo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+ });
+
+ app.get('/view/:id', async (req, res) => {
+  try {
+    const toDo = await ToDo.findById(req.params.id);
+    res.status(200).json(toDo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.put('/list/update/:id', async (req, res) => {
+  try {
+    const {id} = req.params
+    await ToDo.findByIdAndUpdate(id, req.body);
+    const toDo = await ToDo.findById(id);
+    res.status(200).json(toDo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+ });
+
+ app.delete('/list/:id', async (req,res) => {
+  try {
+    await ToDo.findByIdAndDelete(req.params.id);
+    if (!ToDo.findByIdAndDelete(req.params.id)) {
+      res.status(404).send({message: "not found"})
+    }
+    res.status(200).json(await ToDo.findById(id));
+    console.log("Deleted")
+  }
+  catch (error){
+    res.status(400).send(error)
+  }
+ });
+
+
+ app.post('/User/create', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});      
+
+app.get('/User/view', async (req, res) => {
+try {
+  const toDo = await User.find();
+    res.status(200).json(toDo);
+} catch (error) {
+  res.status(400).send(error);
+}
+});
+
+app.get('/User/view/:id', async (req, res) => {
+try {
+  const toDo = await User.findById(req.params.id);
+  res.status(200).json(toDo);
+} catch (error) {
+  res.status(400).send(error);
+}
+});
+
+app.put('/User/update/:id', async (req, res) => {
+try {
+  const {id} = req.params
+  await User.findByIdAndUpdate(id, req.body);
+  const toDo = await User.findById(id);
+  res.status(200).json(toDo);
+} catch (error) {
+  res.status(400).send(error);
+}
+});
+
+app.delete('/User/:id', async (req,res) => {
+try {
+  await User.findByIdAndDelete(req.params.id);
+  if (!User.findByIdAndDelete(req.params.id)) {
+    res.status(404).send({message: "not found"})
+  }
+  res.status(200).json(await User.findById(id));
+  console.log("Deleted")
+}
+catch (error){
+  res.status(400).send(error)
+}
+});
+
+
+ 
---
+++ schema/Note.model.js.js
@@ -0,0 +1,28 @@
+const Mongoose =require("mongoose");
+const schema = new Mongoose.Schema({
+  id: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  title: {
+    type: String
+  },
+  created_by: {
+    type: String
+  },
+  created_on: {
+    type: Date
+  },
+  contant: {
+    type: String
+  },
+  dead_line: {
+    type: Date
+  },
+  status: {
+    type: String
+  }
+});
+const Model = Mongoose.model('To-Do List', schema);
+module.exports = Model;
